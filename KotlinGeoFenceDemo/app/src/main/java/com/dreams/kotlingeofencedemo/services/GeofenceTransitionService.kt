c1dba6f4493027ea0ab48d0bef3730c09819a583
Tc2r <Nudenniew@gmail.com>
2020-01-22 22:35:43 -0600

    Added code to retrieve geofencetransitiondetails from list of triggering geofence events.

--- KotlinGeoFenceDemo/app/src/main/java/com/dreams/kotlingeofencedemo/services/GeofenceTransitionService.kt
+++ KotlinGeoFenceDemo/app/src/main/java/com/dreams/kotlingeofencedemo/services/GeofenceTransitionService.kt
@@ -2,11 +2,14 @@ package com.dreams.kotlingeofencedemo.services
 
 import android.app.IntentService
 import android.content.Intent
+import android.text.TextUtils
 import android.util.Log
 import com.example.testgeofence.MapsActivity
 import com.example.testgeofence.R
+import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofenceStatusCodes
 import com.google.android.gms.location.GeofencingEvent
+import java.util.*
 
 /**
  * Created by Tc2r on 1/19/2020.
@@ -25,6 +28,41 @@ class GeofenceTransitionService : IntentService(TAG) {
             Log.e(TAG, errorMessage)
             return
         }
+
+        val geoFenceTransition = geofencingEvent.geofenceTransition
+
+        // Check if the transition type is of interest.
+        if (geoFenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER || geoFenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            // Get the geofence that was triggered
+            val triggeringGeofences = geofencingEvent.triggeringGeofences
+
+            val geofenceTransitionDetails = getGeofenceTransitionDetails(geoFenceTransition,
+                triggeringGeofences
+            )
+
+            // Send notification details as a String.
+            TODO( "Send Notification")
+        }
+    }
+
+    // Gets the geofenceTransitionDetails from list of triggering events.
+    private fun getGeofenceTransitionDetails(geoFenceTransition: Int,
+                                             triggeringGeofences: List<Geofence>): String
+    {
+        // get the ID of each geofence Triggered
+        val triggeringGeofencesList = ArrayList<String>()
+        for (geofence in triggeringGeofences) {
+            triggeringGeofencesList.add(geofence.requestId)
+        }
+
+        var status: String? = null
+        if (geoFenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+            status = "Entering"
+        } else if (geoFenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+            status = "Exiting"
+        }
+
+        return status!! + TextUtils.join(", ", triggeringGeofencesList)
     }
 
 
