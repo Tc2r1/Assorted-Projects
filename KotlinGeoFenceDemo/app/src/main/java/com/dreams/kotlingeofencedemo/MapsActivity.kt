56bfbdf694011b0bc596f811ea8633df8017ec85
Tc2r <Nudenniew@gmail.com>
2020-01-22 20:10:19 -0600

    Added code to call Permission checks at start of application.

--- KotlinGeoFenceDemo/app/src/main/java/com/dreams/kotlingeofencedemo/MapsActivity.kt
+++ KotlinGeoFenceDemo/app/src/main/java/com/dreams/kotlingeofencedemo/MapsActivity.kt
@@ -26,6 +26,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        // Check for and request required permissions.
+        setPermissions()
+        
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
